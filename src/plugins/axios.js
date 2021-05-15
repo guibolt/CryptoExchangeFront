b7b6f7b4668709a0f32536c969f6dca809ebd41d
Guilherme Dos Reis <guibolt@outlook.com>
2021-05-15 20:40:43 -0300

    atualizando url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const httpClient = axios.create({
-    baseURL: "https://localhost:44390/api/v1/Coins",
+   // baseURL: "https://localhost:44390/api/v1/Coins",
+   baseURL: "https://cryptoexchanges.azurewebsites.net/api/v1/Coins",
     headers: {
       "Content-Type": "application/json",
     },
